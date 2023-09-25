4b8e818120b105d78afc477b622239a901f8942a
hsinhoyeh <yhh92u@gmail.com>
2023-09-25 11:04:59 +0800

    stage

---
+++ app/recomsys/modelinf/build_matrix.py
@@ -0,0 +1,76 @@
+# reference: https://www.geeksforgeeks.org/recommendation-system-in-python/
+
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+  
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+  
+ratings = pd.read_csv("https://s3-us-west-2.amazonaws.com/recommender-tutorial/ratings.csv")
+ratings.head()
+  
+movies = pd.read_csv("https://s3-us-west-2.amazonaws.com/recommender-tutorial/movies.csv")
+movies.head()
+  
+n_ratings = len(ratings)
+n_movies = len(ratings['movieId'].unique())
+n_users = len(ratings['userId'].unique())
+  
+print(f"Number of ratings: {n_ratings}")
+print(f"Number of unique movieId's: {n_movies}")
+print(f"Number of unique users: {n_users}")
+print(f"Average ratings per user: {round(n_ratings/n_users, 2)}")
+print(f"Average ratings per movie: {round(n_ratings/n_movies, 2)}")
+  
+user_freq = ratings[['userId', 'movieId']].groupby('userId').count().reset_index()
+user_freq.columns = ['userId', 'n_ratings']
+user_freq.head()
+  
+  
+# Find Lowest and Highest rated movies:
+mean_rating = ratings.groupby('movieId')[['rating']].mean()
+# Lowest rated movies
+lowest_rated = mean_rating['rating'].idxmin()
+movies.loc[movies['movieId'] == lowest_rated]
+# Highest rated movies
+highest_rated = mean_rating['rating'].idxmax()
+movies.loc[movies['movieId'] == highest_rated]
+# show number of people who rated movies rated movie highest
+ratings[ratings['movieId']==highest_rated]
+# show number of people who rated movies rated movie lowest
+ratings[ratings['movieId']==lowest_rated]
+  
+## the above movies has very low dataset. We will use bayesian average
+movie_stats = ratings.groupby('movieId')[['rating']].agg(['count', 'mean'])
+movie_stats.columns = movie_stats.columns.droplevel()
+  
+# Now, we create user-item matrix using scipy csr matrix
+from scipy import sparse
+from scipy.sparse import csr_matrix
+  
+def create_matrix(df):
+      
+    N = len(df['userId'].unique())
+    M = len(df['movieId'].unique())
+      
+    # Map Ids to indices
+    user_mapper = dict(zip(np.unique(df["userId"]), list(range(N))))
+    movie_mapper = dict(zip(np.unique(df["movieId"]), list(range(M))))
+      
+    # Map indices to IDs
+    user_inv_mapper = dict(zip(list(range(N)), np.unique(df["userId"])))
+    movie_inv_mapper = dict(zip(list(range(M)), np.unique(df["movieId"])))
+      
+    user_index = [user_mapper[i] for i in df['userId']]
+    movie_index = [movie_mapper[i] for i in df['movieId']]
+  
+    X = csr_matrix((df["rating"], (movie_index, user_index)), shape=(M, N))
+      
+    return X, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper
+  
+X, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper = create_matrix(ratings)
+sparse.save_npz("rating-matrix.npz", X)
---
+++ app/recomsys/modelinf/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+License: MIT License
+Copyright (c) 2023 Miel Donkers
+Very simple HTTP server in python for logging requests
+Usage::
+    ./server.py [<port>]
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+import json
+import logging
+import requests
+import time
+import urllib.parse
+
+from inference import find_similar_movies
+from scipy import sparse
+from scipy.sparse import csr_matrix
+from urllib.parse import parse_qs
+
+userprofile_url = "http://userprofile.default.svc.cluster.local"
+X = sparse.load_npz("rating-matrix.npz")
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_POST(self):
+        start_time = time.time()
+
+        pathparam = parse_qs(self.path.partition('?')[-1])
+        logging.info(pathparam)
+        userid = pathparam['userid'][0] if 'userid' in pathparam else ""
+        # fetch user profile
+        path_params = {
+            'userid': userid,
+        }
+        prefetch_url = "{}?{}".format(userprofile_url, urllib.parse.urlencode(path_params))
+        logging.info("prefetch url: {}".format(prefetch_url))
+        requests.get(prefetch_url)
+
+        # prepare the payload for inference
+        found = find_similar_movies(3, self.X, k=10)
+        logging.info("recomsys:{}".format(found))
+
+        self._set_response()
+        self.wfile.write("POST request for {} cost {}".format(self.path, time.time() - start_time).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
